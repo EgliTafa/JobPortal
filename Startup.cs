0d3cb4b7d923ed03962cab60cbaf9ca3a7086264
EgliTafa <eglitafa88@gmail.com>
2022-11-08 11:30:51 +0100

    Reworked the ALL Applicants page.

--- Startup.cs
+++ Startup.cs
@@ -43,6 +43,12 @@ namespace JobPortal
             )
                .AddEntityFrameworkStores<ApplicationDbContext>();
 
+            services.Configure<IdentityOptions>(opts => {
+                opts.Password.RequiredLength = 8;
+                opts.Password.RequireDigit = true;
+                opts.Password.RequireLowercase = true;
+            });
+
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
